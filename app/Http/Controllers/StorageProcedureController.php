f8022e93a317bad7652045f834096ab3287c9c71
ismael castro <ismael_castro@outlook.com.br>
2019-09-13 07:54:37 -0300

    Relc123

--- app/Http/Controllers/StorageProcedureController.php
+++ app/Http/Controllers/StorageProcedureController.php
@@ -8,7 +8,10 @@ use Illuminate\Support\Facades\DB;
 class StorageProcedureController extends Controller
 {
     public function c123_faturamento(){
-    	$faturamento = DB::select(DB::raw('call dbo.spRelC123(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)', [0,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,"01/07/2019","31/07/2019","S",-1,-1,-1,"S"]));
+    
+    	$params = [0,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1, '01-06-2019', '30-06-2019',"S", -1,-1,-1, "S"];
+    	$faturamento = DB::select('SET NOCOUNT ON; EXEC dbo.spRelC123 ?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?', $params);
     	dd($faturamento);
+    	
     }
 }
